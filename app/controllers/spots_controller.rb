0b134df8beab822f55aeb3c405c8789fcd764b7f
muhamadzaman <zamandev.us@gmail.com>
2023-03-09 14:26:18 +0500

    checking if images are not null

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -16,7 +16,7 @@ class SpotsController < ApplicationController
 
     def create
         spot = Spot.new(spot_params)
-        spot.images = upload_images(spot_params[:images]) unless spot_params[:images]
+        spot.images = upload_images(spot_params[:images]) unless spot_params[:images].blank?
 
         if spot.save
             render json: { message: "Spot Added Successfully" }, status: :ok
@@ -26,7 +26,7 @@ class SpotsController < ApplicationController
     end
 
     def update
-        images = upload_images(params[:images]) unless params[:images]
+        images = upload_images(params[:images]) unless params[:images].blank?
         if @spot.update(spot_params.merge({images: images}))
             render json: { message: "Spot updated Successfully" }, status: :ok
         else
